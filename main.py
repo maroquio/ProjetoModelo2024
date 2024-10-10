9cad2fbb6151cd9a448cf472da1f2081b03e4958
Ricardo Maroquio <maroquio@gmail.com>
2024-10-09 21:19:57 -0300

    configurada a pasta de arquivos estaticos da aplicacao

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
